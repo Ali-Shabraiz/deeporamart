fd3d370b31ad90c50bfc0ca81079ff22a1b096ca
= <alishabraiz154@gmail.com>
2025-11-24 07:57:01 -0800

    Spell connected fix

--- PHP/fetch_all_users.php
+++ PHP/fetch_all_users.php
@@ -2,8 +2,8 @@
     header("Content-type: Application/JSON");
     include "config.php";
     $query = "SELECT * FROM `users`";
-    $result = mysqli_query($connection, $query) or die("Failed to run Query");
+    $result = mysqli_query($conn, $query) or die("Failed to run Query");
     $assoc = mysqli_fetch_all($result,MYSQLI_ASSOC);
     echo json_encode($assoc);
-    mysqli_close($connection);  
+    mysqli_close($conn);  
 ?>

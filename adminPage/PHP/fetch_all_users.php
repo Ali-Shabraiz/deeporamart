a644e6c800597b25d3a0f3e343d3ec224d60b269
deep raza <deeporamart@gmail.com>
2025-12-06 19:07:39 +0500

    shoUser repo change

---
+++ adminPage/PHP/fetch_all_users.php
@@ -0,0 +1,9 @@
+<?php
+    header("Content-type: Application/JSON");
+    include "config.php";
+    $query = "SELECT * FROM `users`";
+    $result = mysqli_query($conn, $query) or die("Failed to run Query");
+    $assoc = mysqli_fetch_all($result,MYSQLI_ASSOC);
+    echo json_encode($assoc);
+    mysqli_close($conn);
+?>

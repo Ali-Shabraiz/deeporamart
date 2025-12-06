94cd8d1c79b14df1589a779776b023999c5ee727
deep raza <deeporamart@gmail.com>
2025-12-06 20:20:26 +0500

    alomost logIn function is fixed

--- adminPage/PHP/logIn.php
+++ adminPage/PHP/logIn.php
@@ -1,20 +1,18 @@
 <?php
 include "../../PHP/config.php";
-    $name = $_POST['Name'];
-    $pass = $_POST['pass'];
-    $email = $_POST['email'];
-    echo $name;
-    echo $pass;
-    echo $email;
-    $stmt = $conn->prepare("SELECT * FROM our_staff WHERE password = ?");
-    $stmt->bind_param("s", $pass);
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $name = $_GET['Name'];
+    $pass = $_GET['pass'];
+    $email = $_GET['email'];
+}
+    $stmt = $conn->prepare("SELECT * FROM our_staff WHERE password = ? AND name = ? AND email = ?");
+    $stmt->bind_param("sss", $pass,$name,$email);
     $stmt->execute();
     $result = $stmt->get_result();
-
 if ($result->num_rows > 0) {
      session_start();
-     $_SESSION['user'] = $_POST['Name'];
-     header('Location: ./data.html');
+     $_SESSION['user'] = $_GET['Name'];
+     header('Location: ../data.php');
 } else {
     echo "No record found.";
 }

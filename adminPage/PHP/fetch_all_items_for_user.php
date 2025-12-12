98e76782fd4970e9442b0a3d643b6c45110c56a0
deep raza <deeporamart@gmail.com>
2025-12-12 16:22:56 +0500

    making Share page Design

---
+++ adminPage/PHP/fetch_all_items_for_user.php
@@ -0,0 +1,39 @@
+<?php
+header("Content-Type: application/json");
+include "../../PHP/config.php";
+session_start();
+
+// Check if 'tt' parameter is set
+
+    // Prepare and execute the token check
+   
+
+
+    // Use get_result for safer fetching
+    // $result = $stmt->get_result();
+    // print_r($result);
+    
+    // $row = $result->fetch_assoc();
+    // if (mySqli_num_rows) {
+        // Token exists, fetch stock data
+
+
+$query = "SELECT Name,imgPath,sp FROM stock";
+        $res = $conn->query($query);
+
+        if ($res) {
+            echo json_encode($res->fetch_all(MYSQLI_ASSOC));
+        } else {
+            echo json_encode([
+                "code" => 500,
+                "message" => "Server Error"
+            ]);
+        }
+    
+
+$conn->close();
+
+    // Clean up
+    // $stmt->close();
+   
+?>
